64693112a7e33d01ae547ec3b64a5872d9b502f3
laura-vincent <lauravincent78500@gmail.com>
2016-09-13 12:44:31 +0200

    modification validation

--- validation.php
+++ validation.php
@@ -7,7 +7,9 @@ session_start();
 if(	isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password']) && 
 	!empty($_POST['username']) && !empty($_POST['email']) && !empty($_POST['password'])) {
 
-	// TODO
+	UserRegistration( $db, $_POST['username'], $_POST['email'], $_POST['password']);
+	header('Location: login.php');
+
 
 }else{ 
 	$_SESSION['message'] = 'Erreur : Formulaire incomplet';
